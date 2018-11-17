f95bec77fb945a415d36e6b86c0594eb077ec1ae
Mefteg <gimenez.tom@gmail.com>
2018-11-17 12:41:53 +0100

    - Move french local file.

--- app/src/main/java/com/ladybugriders/mumassistant/MainActivity.java
+++ app/src/main/java/com/ladybugriders/mumassistant/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.Date;
@@ -36,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
                 updateViews();
             }
         });
+
+        ListView feedingsListView = findViewById(R.id.feedings_listview);
 
         updateViews();
     }
